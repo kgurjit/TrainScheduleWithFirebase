ca488cf6fd04915ef8b06efb66e2373d37ca8c08
Gurjit Kaur <kgurjit@gmail.com>
2016-06-19 17:56:58 -0400

    Misc changes to edit and delete schedules

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,11 +12,20 @@ $(document).ready(function(){
 		  	firebase.database().ref('schedules/').on('child_added', function(snapshot) {
 			  updateTable(snapshot.key, snapshot.val());
 	 	  	});	
+
+		  	firebase.database().ref('schedules/').on('child_removed', function(snapshot) {
+			  $("tr[data-key='" + snapshot.key + "']").remove();
+	 	  	});	
+
+	 	  	firebase.database().ref('schedules/').on('child_changed', function(snapshot) {
+			  updateTable(snapshot.key, snapshot.val());
+	 	  	});	
+
 		  };
 		  
 		  addUpdateListener();
 		  
-		  var addTrainSch = function(){
+		  var saveSch = function(){
 		  	var trainName = $("#trainName").val();
 		  	var destination = $("#destination").val();
 		  	var firstTrainTime = $("#firstTrainTime").val();
@@ -26,9 +35,13 @@ $(document).ready(function(){
 		  		$("#errorModal").modal();
 		  		return;
 		  	}
+		  	var schKey = $("#key").val();
 
-		  	var newSchKey = firebase.database().ref().child('/schedules/').push().key;
-		  	var resource = '/schedules/' + newSchKey;
+		  	if(schKey === '') {
+		  		schKey = firebase.database().ref().child('/schedules/').push().key;
+		  	} 
+
+		  	var resource = '/schedules/' + schKey;
 		  	var newSch = {
 			    trainName:trainName,
 			    destination:destination,
@@ -37,14 +50,47 @@ $(document).ready(function(){
 		  	};
 
 		  	var update = {};
-		  	update['/schedules/' + newSchKey] = newSch;
+		  	update['/schedules/' + schKey] = newSch;
 			firebase.database().ref().update(update);
+			clearAll();
 		  };
 
 		  $("#addTrainNameBtn").on("click",function(e){
 		  	e.preventDefault();
-		  	addTrainSch();
+		  	saveSch();
 		 });
+
+		  $("tbody").on("click", ".deleteSch", function(e){
+		  	e.preventDefault();
+		  	if(confirm('Are you sure?')){
+		  		var key = $(this).data("id");
+		  		firebase.database().ref('/schedules').child(key).remove();
+		  	}
+		  });
+
+		  $("tbody").on("click", ".editSch", function(e){
+		  	e.preventDefault();
+		  	var key = $(this).data("id");
+		  	var tr = $("tr[data-key='" + key + "']");
+		  	var cols = tr.find('td');
+		  	$("#trainName").val(cols.eq(0).html());
+		  	$("#destination").val(cols.eq(1).html());
+		  	$("#firstTrainTime").val(tr.data('firsttime'));
+		  	$("#frequency").val(tr.data('freq'));
+		  	$("#key").val(key);
+		  });
+
+		  $("#cancelBtn").on("click", function(){
+		  	clearAll();
+		  });
+
+		  var clearAll = function(){
+		  	$("#trainName").val('');
+		  	$("#destination").val('');
+		  	$("#firstTrainTime").val('');
+		  	$("#frequency").val('');
+		  	$("#key").val('');
+		  };
 
 		var updateTable = function(key, data){
 			var freq = data["frequency"];
@@ -63,6 +109,13 @@ $(document).ready(function(){
 			newRow.append($("<td></td>").html(data["frequency"]));
 			newRow.append($("<td></td>").html(moment(nextTrain).format("hh:mma")));
 			newRow.append($("<td></td>").html(minutesTillTrain));
+			if(window.location.href.indexOf('manage.html') > -1) {
+				var delAnchor = $("<a>Delete</a>").attr("href","#").attr("class", "row-action deleteSch").attr("data-id", key);
+
+				var editAnchor = $("<a>Edit</a>").attr("href","#").attr("class", "editSch").attr("data-id", key);
+
+				newRow.append($("<td></td>").append(delAnchor).append().append(editAnchor));
+			}
 
 			newRow.appendTo("#data");
 		};
